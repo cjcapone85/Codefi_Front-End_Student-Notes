6bf71c107fa36faeca7db00dcf6cdbd98df638e1
cjcapone85 <99698474+cjcapone85@users.noreply.github.com>
2022-02-28 12:37:42 -0600

    Revert "Class Exercises"
    This reverts commit 35e78d15ea7c5ccc87c8492550a80be4c6c92d42.

--- C02__JavaScript-Foundations-Part-1__JavaScript/~Starter Code/types_and_variables.js
+++ C02__JavaScript-Foundations-Part-1__JavaScript/~Starter Code/types_and_variables.js
@@ -1,64 +1,13 @@
 // * string * \\
-//"4";
-//"vehicle";
+
 // * number * \\
-//2;
-//50;
 
 // * boolean * \\
-//true;
-//false;
 
 // * special * \\
-//null;
-//NaN;
-//undefined;
 
 // * object * \\
-//car= {
- //   make: Nissan,
-//    model: Altima
-//};
-//car.model="Altima"
 
 // * array * \\
-//food=["pizza", "cake", "carrots"];
-//food[1] = "cake"
+
 // * Declaring Variables * \\
-
-//let mood;
-//mood = "exhausted";
-
-
-const groceryList = [
-    {name: 'Apples', price: '30'},
-    {name: 'Bananas', price: '30'},
-    {name: 'Pears', price: '30'},
-    {name: 'Spinach', price: '30'},
-    {name: 'Chips', price: '30'},
-];
-
-const foodList= document.querySelector(".food-list")
-
-for(let item of groceryList) {
-
-    const listItem = document.createElement('li');
-    console.log("List Item", listItem)
-    listItem.innerText = item.name;
-    console.log(listItem);
-    foodList.appendChild(listItem);
-}
-
-
-
-/*[ ] Create a list of objects, each having a food name and its price
-
- [ ] Loop over that list, dynamically create a new list item `<li></li>` with the food name inside, and append that item to an existing HTML `<ul></ul>` element
-// 1. Loop over the groceryList
-// 2. Create an <li></li> element
-// 3. Place the current food inside the <li></li> element
-// 4. Append <li></li> to existing <ul></ul>
-// 5. Create or Link to a <ul></ul> element
- */
-
-
--- C04__Building-Real-Things__JavaScript/~Starter Code/js/scrolling-progress.js
+++ C04__Building-Real-Things__JavaScript/~Starter Code/js/scrolling-progress.js
@@ -1,11 +1,5 @@
 // * ========== HTML Element Selectors ========== * \\
-const progressBar = document.querySelector("#progress-bar");
 
 // * ========== Function Declarations ========== * \\
-function fillProgressBar() {
-    const windowHeight=window.innerHeight;
-    const fullHeight;document.body.clientHeight;
-    const scrolled=window.scrollY;
-}
+
 // * ========== Event Listeners ========== * \\
-window.addEventListener("scroll", fillProgressBar);
